e05b718019e26a124372e726b43b0ed8ce89a015
alessia263456 <alessiamihaila05@gmail.com>
2026-03-08 13:50:15 +0200

    Lab 1

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Student {
+    Integer numarMatricol;
+    String prenume, nume, formatieDeStudiu;
+    Student(Integer numarMatricol, String prenume, String nume, String formatieDeStudiu)
+    {
+        this.numarMatricol=numarMatricol;
+        this.prenume=prenume;
+        this.nume=nume;
+        this.formatieDeStudiu=formatieDeStudiu;
+    }
+
+    @Override
+    public String toString() {
+        return numarMatricol.toString()+" "+prenume+" "+nume+" "+formatieDeStudiu;
+    }
 }
