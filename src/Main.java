7e196f7b885048e33ad9834e2700a6e2fb1f917e
alessia263456 <alessiamihaila05@gmail.com>
2026-03-08 13:50:51 +0200

    Lab 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,5 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     Student s1=new Student(1234,"Alessia","Mihaila","ISM");
-    System.out.println(s1.toString());
+    System.out.println(s1);
 }
