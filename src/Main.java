397298d6687b1afa505843b4859419c55cd6da68
alessia263456 <alessiamihaila05@gmail.com>
2026-03-08 13:58:45 +0200

    Lab 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     Student s1=new Student(1234,"Alessia","Mihaila","ISM");
     System.out.println(s1);
